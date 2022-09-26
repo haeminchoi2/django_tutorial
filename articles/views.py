0587bee380d459c8f7a48038c870e77447252841
haeminchoi2 <sjhk02@gmail.com>
2022-09-26 21:09:42 +0900

    DTL complete

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def dinner(request, name):
+    menus = [{"name" : '족발',
+              "price" : 30000},
+             {"name" : '햄버거',
+              "price" : 5000},
+             {"name" : '치킨',
+              "price" : 20000},
+             {"name" : '초밥',
+              "price" : 15000}]
+    pick = random.choice(menus)
+    context = {
+        'pick' : pick,
+        'name' : name,
+        'menus' : menus,
+        
+    }
+    return render(request, 'dinner.html', context)
