709503bd61c2be4d9fcb3137fa699163d689ad29
haeminchoi2 <sjhk02@gmail.com>
2022-09-27 21:22:22 +0900

    Turtle DB, ORM, Model, Admin Complete

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model): # 상속
+    # id는 자동으로 만들어 진다.
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
